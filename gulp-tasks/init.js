37934bda133bf7b6ad583435b45defe01062ce38
Yuval Maman <yuvilio@yahoo.com>
2016-01-24 01:16:07 -0500

    start compiling init templates

--- gulp-tasks/init.js
+++ gulp-tasks/init.js
@@ -6,15 +6,21 @@ module.exports = function(opts){
 
 
   opts.gulp.task('init', function(){
-    var labpath = './lab/' + opts.config.labpath;
-    var distpath = './dist/' + opts.config.labpath;
+    var labpath = 'lab/' + opts.config.labpath;
+    var distpath = 'dist/' + opts.config.labpath;
 
     //make the directories we'll need
     var filePaths = [labpath, distpath];
     var mkdirs = filePaths.map(fsp.mkdirp);
-
     Promise.all(mkdirs).then(function(){
       console.log('made directories');
+    }).then(function(){ //then copy starter files
+
+      opts.gulp.src('templates/sample/_layouts/**/*.nunj')
+        .pipe($.nunjucks.compile({distpath: distpath, labpath: labpath}))
+        .pipe(opts.gulp.dest(labpath + '/_layouts'));
+
+
     }).catch(function(er){
       console.log(er);
     });
